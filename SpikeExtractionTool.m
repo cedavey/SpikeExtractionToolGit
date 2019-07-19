af5248467c9a359fc42d4a3d1b82e5a64a77a4d9
srarty <artemio.sotobreceda@users.noreply.github.com>
2019-07-19 14:30:36 +1000

    Help
    + Added a dummy PDF that is open when Help is clicked

--- SpikeExtractionTool.m
+++ SpikeExtractionTool.m
@@ -2266,7 +2266,7 @@ function helpMenuItem_Callback(hObject, eventdata, handles)
 % hObject    handle to helpMenuItem (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
+   open('resources\SEThelp.pdf');
 end
 
 % --------------------------------------------------------------------
